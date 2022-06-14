aca3163a8f410c886db68584b7b30d33fd54f380
Akshay Yeole <akshayyeole4366@gmail.com>
2022-06-14 10:34:08 +0530

    Add project files.

---
+++ Web_Api_Core_Api_Versioning/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_Api_Core_Api_Versioning.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public ActionResult<string> Index()
+        {
+            return "Inside Home controller";
+        }
+    }
+}
